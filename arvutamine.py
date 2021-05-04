320a1b25dbfcc6cf851e67846d658507fee7d2eb
Niklas <niklas.wojtkowiak@real.edu.ee>
2021-05-04 10:22:03 +0300

    Initial Commit
    first commit to the repository

---
+++ arvutamine.py
@@ -0,0 +1,30 @@
+import random
+
+
+def quizQuestion():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    score = (0)
+    answer = int(input("How much is " + str(num1) + " + " + str(num2) + " ? "))
+
+    if answer == num1+num2:
+         score = (score+1)
+
+         print("Correct!")
+         print("Score:", score)
+
+    else:
+
+        print("Wrong! The correct answer was " + str(num1+num2))
+
+
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
+quizQuestion()
